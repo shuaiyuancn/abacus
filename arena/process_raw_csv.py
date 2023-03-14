bd4250647830e1b4b1c7aae81d5faf5b58155a7c
Shuai Yuan <me@yuan-shuai.info>
2023-03-14 18:07:21 +0000

    processed raw data

---
+++ arena/process_raw_csv.py
@@ -0,0 +1,33 @@
+# %%
+import pandas as pd
+
+# %%
+df = pd.read_csv('../dataset/VEVE-raw.csv')
+
+# %%
+df.head()
+
+# %%
+df.dtypes
+
+# %%
+from datetime import datetime
+import functools
+
+df['Date'] = df['Date'].apply(lambda x: datetime.strptime(x, '%b %d, %Y'))
+
+# %%
+def convert_volume_to_float(vol):
+    if vol == '-':
+        return 0.0
+    else:
+        return float(
+            vol.replace(',', '')
+        )
+
+df['Volume'] = df['Volume'].apply(convert_volume_to_float)
+
+# %%
+df.to_csv('../dataset/VEVE-processed.csv', index=None)
+
+# %%
